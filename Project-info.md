88a322225387cec606c7dcafbdcef492971c7fae
sunnyjacob <sunny@visualise.it>
2024-12-28 16:04:46 +0000

    Add project information and generate Convex API and schema files
    - Created `Project-info.md` to document project details, including domain and links to GitHub repositories and services (Convex, Clerk, Resend).
    - Ran npx convex dev
    - Added schema definition in `schema.ts` for tables: providers, services, business hours, and appointments, including their respective fields and indexes.

---
+++ Project-info.md
@@ -0,0 +1,22 @@
+# Project Info
+
+domain: https://getmebooked.in
+
+Github template repo: https://github.com/visualiseIT/booki
+
+Github repo: https://github.com/visualiseIT/getmebooked (not yet created)
+
+
+Convex db has been created. 
+
+https://dashboard.convex.dev/t/sunny-a1794/getmebooked/wooden-whale-254/data
+
+Clerk has been created. 
+
+https://dashboard.clerk.com/apps/app_2qDS6YCgicJV7MXeBuuYuzdZXBR/instances/ins_2qDS6c3NS7Ecadpx64wHoWLg6yV
+
+Resend has been created. 
+
+https://resend.com/domains
+
+
---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,42 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as appointments from "../appointments.js";
+import type * as businessHours from "../businessHours.js";
+import type * as providers from "../providers.js";
+import type * as services from "../services.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  appointments: typeof appointments;
+  businessHours: typeof businessHours;
+  providers: typeof providers;
+  services: typeof services;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
